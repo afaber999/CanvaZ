84bf0209cdcc9beecb50b8f4dca1893117b189ea
afaber999 <albert.faber@philips.com>
2024-11-04 23:15:58 +0100

    move to seperate file

--- src/main.zig
+++ src/main.zig
@@ -1,121 +1,14 @@
 const std = @import("std");
-
-const c = @cImport({
-    @cInclude("windows.h");
-});
-
-
-var g_hwnd: c.HWND = null;
-
-pub fn canvasWndProc(hwnd: c.HWND, msg: c.UINT, wParam: c.WPARAM, lParam: c.LPARAM) callconv(.C) c.LRESULT {
-
-    std.debug.print("---- canvasWndProc {any}]\n", .{msg});
-    return switch (msg) {
-        else => c.DefWindowProcA(hwnd, msg, wParam, lParam),    
-    };
-}
-
-fn open_window(name : [:0]const u8, width : i32, height :i32) !void {
-    const instance = c.GetModuleHandleA(null);
-    std.debug.print("Hinstance {any}", .{instance});
-
-    var wc = std.mem.zeroes(c.WNDCLASSEX);
-    wc.cbSize = @sizeOf(c.WNDCLASSEX);
-    wc.style = c.CS_VREDRAW | c.CS_HREDRAW;
-    wc.lpfnWndProc = canvasWndProc;
-    wc.hInstance = instance;
-
-    wc.lpszClassName = name;
-    _ = c.RegisterClassExA(&wc);
-
-    const dwStyle = c.WS_OVERLAPPEDWINDOW & ~c.WS_MAXIMIZEBOX & ~c.WS_MINIMIZEBOX | c.WS_VISIBLE;
-
-//   // Calculate the required size of the window rectangle based on desired client area size
-    const window_rect = c.RECT {
-        .left = 0,
-        .top = 0,
-        .right = width,
-        .bottom = height,
-    };
-    //_ = c.AdjustWindowRectEx(&window_rect, dwStyle, c.FALSE, 0);
-
-
-    g_hwnd = c.CreateWindowExA(
-            c.WS_EX_CLIENTEDGE, name, name,
-                            dwStyle, c.CW_USEDEFAULT, c.CW_USEDEFAULT,
-                            window_rect.right - window_rect.left + 4,
-                            window_rect.bottom - window_rect.top + 4,
-                            null, null, instance, null);
-
-    if (g_hwnd == null) {
-    const error_code = c.GetLastError();
-    var buffer: [256]u8 = undefined;
-    const message_length = c.FormatMessageA(
-        c.FORMAT_MESSAGE_FROM_SYSTEM | c.FORMAT_MESSAGE_IGNORE_INSERTS,
-        c.NULL,
-        error_code,
-        0,
-        &buffer[0],
-        @as(u32, @intCast( buffer.len)),
-        null,
-    );
-    if (message_length > 0) {
-        std.debug.print("\n\nCreateWindowExA failed with error: {s}\n", .{buffer[0..message_length]});
-    } else {
-        std.debug.print("\n\nCreateWindowExA failed with error code: {d}\n", .{error_code});
-    }
-
-        return error.HandleInvalid;
-        //return error.WindowsError{.code = c.GetLastError()};
-        //return c.GetLastError();
-    }
-
-    _ = c.ShowWindow(g_hwnd, c.SW_NORMAL);
-    _ = c.UpdateWindow(g_hwnd);
-}
-
-fn fenster_loop() i32 {
-  
-  var msg: c.MSG = undefined;
-
-  while (c.PeekMessageA(&msg, null, 0, 0, c.PM_REMOVE) != 0 ) {
-    if (msg.message == c.WM_QUIT)
-      return -1;
-    _ = c.TranslateMessage(&msg);
-    _ = c.DispatchMessageA(&msg);
-  }
-
-  // request reqpaint
-  _ = c.InvalidateRect(g_hwnd, null, c.TRUE);
-  return 0;
-}
-
-
-
+const CanvaZ = @import("CanvaZ.zig");
 
 
 pub fn main() !void {
+ 
+    var canvas = CanvaZ.init();
+
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    try open_window("AAAABCD", 800,600);
+    try canvas.createWindow("CanvaZ Demo", 800,600);
 
-    while (fenster_loop() == 0) {
+    while (canvas.update() == 0) {
     }
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
